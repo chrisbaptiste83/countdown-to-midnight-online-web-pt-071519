27daefdc07953fff69b59f503d12c61e18ee0e5a
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-07-16 13:39:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,12 @@
 #write your code here
 
 def countdown(nnumber)
-  
+  while number > 0
+   sleep 1
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
   puts 
 end
